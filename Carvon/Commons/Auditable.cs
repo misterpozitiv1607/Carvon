c11c94feb2872e24c1b1fd8fece125084772b84a
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-12-16 04:45:35 +0500

    Error connection

--- Carvon/Commons/Auditable.cs
+++ Carvon/Commons/Auditable.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Carvon.Commons;
 
-namespace Carvon.Commons
+public class Auditable
 {
-    internal class Auditable
-    {
-    }
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
