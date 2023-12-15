09f549057c45c0f29153f6af54c1aa374649f763
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-12-16 04:55:22 +0500

    Entities created

---
+++ Carvon/Products/ProductCategory.cs
@@ -0,0 +1,10 @@
+﻿using Carvon.Commons;
+
+namespace Carvon.Products;
+
+public class ProductCategory : Auditable
+{
+    public string Name { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+}
